0d7585582acc4b87064ad41149c47ad5bd132bd5
Sameer Kulkarni <samgrk14@gmail.com>
2026-03-25 17:01:54 +0530

    click for the code

---
+++ functions/function.py
@@ -0,0 +1,39 @@
+# #func def
+
+# def calc_sum(a,b):
+#     return a+b
+
+# sum=calc_sum(1,2)
+# print(sum)
+
+
+
+
+# def print_hello(): # functions without arg 
+#     print("hello world ")
+
+# print_hello() 
+
+
+# agv of three nnumber
+
+# def calc_avg(a , b , c):
+#     sum  =a+b+c
+#     avg = sum / 3
+
+#     print(avg)
+#     #return avg
+
+# calc_avg(90,26,82)
+
+
+
+#mul of 2 num
+# TimeoutError
+# def mul_calc(a , b=2):  # here we set b =2 as constant and we pass the args a single val it will return the value
+#     print(a*b)
+#     return a*b
+
+# mul_calc(883153468462132546)
+
+
